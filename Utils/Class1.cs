5192d05aea4871dc7f1c48fe5a8640d1184eb152
TrofimovMA <svintyc@mail.ru>
2022-05-24 06:34:39 +0300

    1

--- Utils/Class1.cs
+++ Utils/Class1.cs
@@ -19,7 +19,7 @@ namespace Utils
     {
         public static void PrintSomething()
         {
-            Console.WriteLine("PRINT SOMETHING");
+            Console.WriteLine("PRINT SOMETHING 222");
         }
 
         public static bool StartsWithUpper(this string str)
