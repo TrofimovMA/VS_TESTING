0e912628537cfb061f3a0c71a4fa96994080ffdf
TrofimovMA <svintyc@mail.ru>
2022-05-24 06:54:34 +0300

    TITLE
    MSG1
    MSG2
    MSG3

--- Utils/Class1.cs
+++ Utils/Class1.cs
@@ -19,7 +19,7 @@ namespace Utils
     {
         public static void PrintSomething()
         {
-            Console.WriteLine("PRINT SOMETHING 8");
+            Console.WriteLine("PRINT SOMETHING 9");
         }
 
         public static bool StartsWithUpper(this string str)
