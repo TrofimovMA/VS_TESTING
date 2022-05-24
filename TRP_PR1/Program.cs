c3920f991b7743b0cefba97ff28ecb26d836a5df
TrofimovMA <svintyc@mail.ru>
2022-05-24 06:28:33 +0300

    Add project files.

---
+++ TRP_PR1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils;
+
+namespace TRP_PR1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PR1");
+            StringLibrary.PrintSomething();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Utils/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public static class MyExtensions
+    {
+        public static int WordCount(this string str)
+        {
+            return str.Split(new char[] { ' ', '.', '?' },
+                             StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+
+    public static class StringLibrary
+    {
+        public static void PrintSomething()
+        {
+            Console.WriteLine("PRINT SOMETHING");
+        }
+
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            char ch = str[0];
+            return char.IsUpper(ch);
+        }
+    }
+}
